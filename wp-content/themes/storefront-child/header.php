753816d3420e2c26a444afa9160a5554b9b8fffe
Ezequiel <ezequiel.domenech@gmail.com>
2020-03-19 17:05:32 -0300

    terminado estilo header

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -34,7 +34,14 @@
 				<?php
 				if ( storefront_is_woocommerce_activated() ) { ?>
 					<div class="site-search custom-search">
-						<?php the_widget( 'WC_Widget_Product_Search', 'title=' ); ?>
+						<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+							<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'woocommerce' ); ?></label>
+							<i class="fas fa-search"></i>
+							<input type="search" id="product-search-input" class="search-field" placeholder="BUSQUEDA" value="<?php echo get_search_query(); ?>" name="s" />
+							<i class="fab fa-facebook-f"></i><i class="fab fa-instagram"></i><i class="fab fa-linkedin"></i>
+							<button type="submit" class="submit-hidden" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>"><?php echo esc_html_x( 'Search', 'submit button', 'woocommerce' ); ?></button>
+							<input type="hidden" name="post_type" value="product" />
+						</form>
 					</div>
 				<?php } ?>
 			</div>
@@ -42,7 +49,10 @@
 				<img src="<?php echo get_template_directory_uri() . '-child/assets/img/cropped-Logo-cuadrado-scaled.png';?>" alt="logo">
 			</div>
 			<div class="cart-container">
-				<a class="mailto" href="mailto:info@conceptmoda.com.ar">info@conceptmoda.com.ar</a>
+				<div class="sendmail">
+					<i class="fas fa-envelope"></i><a class="mailto" href="mailto:info@conceptmoda.com.ar">info@conceptmoda.com.ar</a>
+				</div>
+				<button class="login">LOGIN </button>
 				<?php custom_header_cart(); ?>
 			</div>
 		</div>
